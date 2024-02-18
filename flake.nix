f8df322856f277f2404b40abb8ce5d48a84a1027
Daniel Eberle <dan.eberle@protonmail.com>
2024-02-18 09:19:34 -0600

    :sparkles: add rust and go devshells

--- flake.nix
+++ flake.nix
@@ -3,12 +3,29 @@
 
   outputs = { self }: {
     templates = {
+
+      devshell = {
+        path = ./devshell;
+        description = "Generic default devshell";
+      };
+
+      rust-project = {
+        path = ./rust-project;
+        description = "Basic Rust+Cargo environment";
+      };
+
+      go-project = {
+        path = ./go-project;
+        description = "Basic Golang environment";
+      };
+
       ansible-config = {
         path = ./ansible-config;
         description = "Per device ansible config template";
       };
+
     };
 
-    defaultTemplate = self.templates.ansible-config;
+    defaultTemplate = self.templates.devshell;
   };
 }
